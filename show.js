a4ff04142e67c7b5d5ca6809b8b7dbbc903f6805
= <jing80714@gmail.com>
2013-12-16 04:12:02 +0800

    rrr

--- show.js
+++ show.js
@@ -1,9 +1,22 @@
-function showrestaurant(a)
+function showrestaurant()
 {
-	output = "店名："+a.name+"<br/>經度："+a.position.latitude+"<br/>緯度："+a.position.longitude+
-	"<br/>電話："+a.phone+"<br/>地址："+a.address+"<br/>分類："+a.category[0]+"<br/>星等："+a.score+"<br/>評價："+a.comments;
-	document.getElementById("demo").innerHTML=output;
-	document.getElementById("demo1").innerHTML=output;
+	//var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+	// Put the object into storage
+	//localStorage.setItem('testObject', JSON.stringify(testObject));
+	var s = localStorage.getItem("res");
+	var r = JSON.parse(s);
+	var count = 0;
+	var max = r.length;
+	//alert(JSON.parse(r));
+		output = "店名："+r.name
+		+"<br/>經度："+r.position.latitude
+		+"<br/>緯度："+r.position.longitude
+		+"<br/>電話："+r.phone
+		+"<br/>地址："+r.address
+		+"<br/>分類："+r.category[0]
+		+"<br/>星等："+r.score;
+		count++;
+		document.getElementById(count).innerHTML=output;
 }
 
 function showcomment(a)
