ee6491225d2e157c49b17f818e9ff1429164f659
radi9 <redbutbray@gmail.com>
2013-12-16 12:36:22 +0800

    new calculate distance Vincenty

--- mapmodule/inmap.js
+++ mapmodule/inmap.js
@@ -69,7 +69,6 @@ function init(){
 				 map : map,
 				 position : userPos,
 				 content : 'Your location here.',
-	             icon : 'http://labs.google.com/ridefinder/images/mm_20_green.png'
 			 });
 			 map.setCenter(userPos);
 		 },function(error){
@@ -172,37 +171,58 @@ function shopMarker(){
 //	return results;
 //}
 
-function calDistance(user,shops,limit){
+function calDistance(lat1, lon1, lat2, lon2){
 	/*
-	 * Great-circle distance formula approach to implement distance
-	 * between two point when given latitude and longitude
-	 * formula :  
+     *Calculates geodetic distance between two points specified 
+     *by latitude/longitude using Vincenty inverse formula for ellipsoids
 	 */
-	google.maps.LatLng.prototype.distanceFrom = function(latlng) {	
-		var lat = [this.lat(), latlng.lat()];
-		var lng = [this.lng(), latlng.lng()];
-		var R = 6378137;
-		var dLat = (lat[1]-lat[0]) * Math.PI / 180;
-		var dLng = (lng[1]-lng[0]) * Math.PI / 180;
-		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		Math.cos(lat[0] * Math.PI / 180 ) * Math.cos(lat[1] * Math.PI / 180 ) *
-		Math.sin(dLng/2) * Math.sin(dLng/2);
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		var d = R * c;
-		return Math.round(d);
-	}
+	// WGS-84 ellipsoid params
+	var a = 6378137;
+	var b = 6356752.314245;
+	var f = 1/298.257223563;
 	
-	var origin = new GLatLng(userCoordinate.latitude,userCoordinate.longitude);
-	var results = new Arrray();
+	var L = (lon2-lon1).toRad();
+	var U1 = Math.atan((1-f) * Math.tan(lat1.toRad()));
+	var U2 = Math.atan((1-f) * Math.tan(lat2.toRad()));
+	var sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
+	var sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
 	
-	for(var i in shops){
-		var destination = new GLatLng(shops[i].position.latitude,shops[i].position.longitude);
-		distance = destination.ditanceFrom(origin);
-		if(distance<limit){
-			results.push(temp.name);
-		}
-	}
-	return results;
+	var lambda = L, lambdaP, iterLimit = 100;
+	
+	do{
+		var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
+		var sinSigma = Math.sqrt((cosU2*sinLambda) * (cosU2*sinLambda) + 
+				(cosU1*sinU2-sinU1*cosU2*cosLambda) * (cosU1*sinU2-sinU1*cosU2*cosLambda));
+		if (sinSigma==0) return 0;  // co-incident points
+		var cosSigma = sinU1*sinU2 + cosU1*cosU2*cosLambda;
+		var sigma = Math.atan2(sinSigma, cosSigma);
+		var sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+		var cosSqAlpha = 1 - sinAlpha*sinAlpha;
+		var cos2SigmaM = cosSigma - 2*sinU1*sinU2/cosSqAlpha;
+		if (isNaN(cos2SigmaM)) cos2SigmaM = 0;  // equatorial line: cosSqAlpha=0 (§6)
+		var C = f/16*cosSqAlpha*(4+f*(4-3*cosSqAlpha));
+		lambdaP = lambda;
+		lambda = L + (1-C) * f * sinAlpha *
+		         (sigma + C*sinSigma*(cos2SigmaM+C*cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)));
+	}while (Math.abs(lambda-lambdaP) > 1e-12 && --iterLimit>0);
+	
+	
+	 if (iterLimit==0) return NaN;  // formula failed to converge
+
+	 var uSq = cosSqAlpha * (a*a - b*b) / (b*b);
+	 var A = 1 + uSq/16384*(4096+uSq*(-768+uSq*(320-175*uSq)));
+	 var B = uSq/1024 * (256+uSq*(-128+uSq*(74-47*uSq)));
+	 var deltaSigma = B*sinSigma*(cos2SigmaM+B/4*(cosSigma*(-1+2*cos2SigmaM*cos2SigmaM)-
+	    B/6*cos2SigmaM*(-3+4*sinSigma*sinSigma)*(-3+4*cos2SigmaM*cos2SigmaM)));
+	 var s = b*A*(sigma-deltaSigma);
+	  
+	 s = s.toFixed(3); // round to 1mm precision
+	 return s;
+	  
+	  // note: to return initial/final bearings in addition to distance, use something like:
+	 var fwdAz = Math.atan2(cosU2*sinLambda,  cosU1*sinU2-sinU1*cosU2*cosLambda);
+	 var revAz = Math.atan2(cosU1*sinLambda, -sinU1*cosU2+cosU1*sinU2*cosLambda);
+	 return { distance: s, initialBearing: fwdAz.toDeg(), finalBearing: revAz.toDeg() };
 }
 
 
