57b8e2e49dc7ed6ecdeee4279dc14f8cf717d482
benson2b <benson2b@hotmail.com>
2014-01-05 01:20:11 +0800

    list update

--- UI/shopinfo/lists.js
+++ UI/shopinfo/lists.js
@@ -1,17 +1,25 @@
 /* show list of shops or comments */
-window.onload = function () {
+function data(rec) {
+	//alert(rec);
 	// get the num of shops/comments first
-    var num = 10;
+    var num = rec.length;
 	// get the arguments
 	var arguments = new Array(num-1);
-	for(var i = 0 ; i < num ; i++)
-		arguments[i] = "commets"+(i+1);
+	for(var i = 0 ; i < num ; i++){
+		arguments[i] = rec[i];
+		//alert(arguments[i]);
+	}
 	var content = document.getElementById("lists");
+	//alert(content);
 	// use innerHTML to add a seperate line
-	content.innerHTML += "<hr></hr>";
+
+	content.innerHTML += "<hr/>";
+	
 	// generate the lists by innerHTML with for loop
-	for(var i = 0 ; i < num; i++){
+	for(var i = 0 ; i < num; i++)
+	{
+		//alert(i);
 		content.innerHTML += "<div>"+arguments[i]+"</div>";
-		content.innerHTML += "<hr></hr>";
+		content.innerHTML += "<hr/>";
 	}
 };
